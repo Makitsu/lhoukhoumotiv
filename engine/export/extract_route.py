9d7d22c176a193c890aa728df6d23d60c2082d8d
lhoumeau <maxime>
2020-04-27 14:07:46 +0200

    correct extraction

--- engine/export/extract_route.py
+++ engine/export/extract_route.py
@@ -5,6 +5,8 @@ start = time.time()
 
 df = pd.read_csv('../../data/temp/train_schedule.csv', sep=';')
 
+stop_df = pd.read_csv('../../data/temp/export_stop.csv', sep=';')
+
 list_station_id = df['stop'].unique().tolist()
 
 extract_df = df.groupby('trip_id')['stop'].apply(list).reset_index()
@@ -20,18 +22,20 @@ result = result[~result['stop'].apply(pd.Series).duplicated()]
 
 #result.to_csv('../../data/temp/connections2.csv',header=True,sep=';',index=False,encoding='utf-8')
 
-foreign_uic = {80110684,80143099,80142281,80140087,80143198,80143313,80143503,80290346,80291039,
-                80021402,80203471,80253914,80196980,80142778,80253914,80196980,#deutchland
-                82001000,#Luxembourg
-                87756403,#Monaco
-                88140010,#Belgique
-                83002022,83016451,83002485,83002048,83002220,83002451#Italie
-               }
+# foreign_uic = {80110684,80143099,80142281,80140087,80143198,80143313,80143503,80290346,80291039,
+#                 80021402,80203471,80253914,80196980,80142778,80253914,80196980,#deutchland
+#                 82001000,#Luxembourg
+#                 87756403,#Monaco
+#                 88140010,#Belgique
+#                 83002022,83016451,83002485,83002048,83002220,83002451#Italie
+#                }
+
 
 storage = {'stop_uic':[],'stop_name':[],'connections':[],'connections_name':[]}
 
+print(stop_df['stop_id'])
 for station in list_station_id:
-    if station not in foreign_uic:
+    if station in stop_df['stop_id'].tolist():
         print('current',station)
         storage['stop_uic'].append(str(station))
         station_name = df[df['stop'] == station]['stop_by_name'].tolist()[0]
@@ -44,7 +48,7 @@ for station in list_station_id:
             if station in list_stop:
                 for other_station in list_stop:
                     if other_station not in connections and other_station != station:
-                        if other_station not in foreign_uic:
+                        if other_station in stop_df['stop_id'].tolist():
                             connections.append(other_station)
                             other_name = df[df['stop'] == other_station]['stop_by_name'].tolist()[0]
                             connections_name.append(other_name)
