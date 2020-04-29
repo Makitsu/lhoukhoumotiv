7ff8b2735e86c14e4721b2723e74c6ba65feba18
Antoine <a.lhoumeau@tourism-academy.com>
2020-04-29 18:32:33 +0200

    modify path in map_tgv.py

--- engine/mapping/map_tgv.py
+++ engine/mapping/map_tgv.py
@@ -54,7 +54,7 @@ def plot_station(origin, connection):
         fg["{}".format(current_origin)] = folium.FeatureGroup(name=current_name, show=False)
         folium_map.add_child(fg["{}".format(current_origin)])
         #marker_cluster["{}".format(current_origin)] = MarkerCluster().add_to(fg["{}".format(current_origin)])
-        icon_path_departure = r"C:\Users\lhoum\Documents\Project\lhoukhoumotiv\engine\mapping\folium_add\station.png"
+        icon_path_departure = r"folium_add\station.png"
         icon_departure = folium.features.CustomIcon(icon_image=icon_path_departure, icon_size=(20, 20))
         popup_text = "<br>{}<br>{}<br>"
         popup_text = popup_text.format(row["stop_name"],
@@ -78,7 +78,7 @@ def plot_station(origin, connection):
                 size = 30
             print(size)
             if row['origin'] == int(current_origin):
-                icon_path = r"C:\Users\lhoum\Documents\Project\lhoukhoumotiv\engine\mapping\folium_add\placeholder.png"
+                icon_path = r"folium_add\placeholder.png"
                 icon_station = folium.features.CustomIcon(icon_image=icon_path, icon_size=(size, size))
             # generate the popup message that is shown on click.
                 popup_text = "<br>{}<br>{}<br>"
