dc2b818058cba24e6a2e8038736453ffadf2f85f
jckhou <jckhou@gmail.com>
2020-05-07 16:07:07 +0200

    add blasons images

--- BeerScrapper/Wikiscrapper/blasonScrapper.py
+++ BeerScrapper/Wikiscrapper/blasonScrapper.py
@@ -6,6 +6,9 @@ from requests_html import HTMLSession
 import os
 from urllib import request
 from time import *
+import csv
+import pandas as pd
+import re
 
 def saveAlphabet(alphabet):
     with open('alphabet', mode='wb') as file:
@@ -23,70 +26,60 @@ def loadAlphabet():
 
 #Scrapper parameters
 INITIATE_alphabet = True
-
-if INITIATE_alphabet is True:
-    alphabet = list(ascii_uppercase)
-    alphabet = alphabet[0:] #Start from C
-    saveAlphabet(alphabet)
-else:
-    alphabet = loadAlphabet()
-
 indexURL = "https://armorialdefrance.fr/liste_alpha.php?initiale="
+Errors = []
 
-nb_total = 0
-while len(alphabet)>0:
-    tic = time()
-    nb_commune = 0
+#Load most populated France communes
+file = pd.read_csv('communes.csv', sep =";",encoding='windows-1252')
 
-    # HTML page loading with java
+#Loop on list of communes
+for i, j in file.iterrows():
+    first_letter = j[2][0]
+
+    # HTML page loading
     session = HTMLSession()  # create an HTML Session object
-    resp = session.get(indexURL+alphabet[0])  # Use the object above to connect to needed webpage
-
+    resp = session.get(indexURL+first_letter)  # Use the object above to connect to needed webpage
     # HTML data extraction
     soup = bs(resp.html.html, 'lxml')
     session.close()
 
-    # retrieve link list for each alphabet letter
-    links = []
+    # retrieve searched city link
     links_page = soup.find(attrs={'class':"en_retrait"})
-    for link in links_page.find_all("a"):
-        links.append("https://armorialdefrance.fr/" + link.get('href'))
+    result = links_page.find("a", string = j[2].upper())
 
-        #iterate in each "commune" page
-        for i in range(0,len(links)):
+    try:
+        link = "https://armorialdefrance.fr/" + result.get('href')
+        print(link)
 
-            # HTML page loading with java
-            session = HTMLSession()  # create an HTML Session object
-            resp = session.get(links[i])  # Use the object above to connect to needed webpage
+        # HTML page loading
+        session = HTMLSession()  # create an HTML Session object
+        resp = session.get(link)  # Use the object above to connect to needed webpage
 
-            # HTML data extraction
-            soup = bs(resp.html.html, 'lxml')
-            session.close()
+        # HTML data extraction
+        soup = bs(resp.html.html, 'lxml')
+        session.close()
 
-            # final data extraction
+        # final data extraction
 
-            #check existence of image directory
-            dirname = os.path.dirname(__file__)
-            blasondirname = dirname + "/images/blasons"
-            test = os.path.exists(blasondirname)
-            if not test:
-                print("new blason directory is created")
-                os.mkdir(blasondirname)
+        #check existence of image directory
+        dirname = os.path.dirname(__file__)
+        blasondirname = dirname + "/images/blasons"
+        test = os.path.exists(blasondirname)
+        if not test:
+            print("new blason directory is created")
+            os.mkdir(blasondirname)
 
-            data = soup.find_all('div',{"id":"corps"})[1]
-            blason_pathname = "images/blasons/" + data.find('b').contents[0].strip().replace("/","-") +".jpg"
-            blason_image_link = data.find('img')['href']
-            request.urlretrieve(blason_image_link, blason_pathname)
+        data = soup.find_all('div',{"id":"corps"})[1]
+        blason_pathname = "images/blasons/" + data.find('b').contents[0].strip().replace("/","-") +".jpg"
+        blason_image_link = data.find('img')['href']
+        request.urlretrieve(blason_image_link, blason_pathname)
 
-            #Following script progress
-            nb_commune +=1
-            if nb_commune % 100 == 0:
-                print(blason_image_link)
+        #Following script progress
+        print(j[2], "has been processed\n")
+    except AttributeError:
+        Errors.append(j[2])
+        pass
 
-        nb_total += nb_commune
-        toc = time()
-        delay = toc - tic
-    print('letter', alphabet[0],'has been processed\n',nb_commune, 'are beginning with this letter\n', nb_total, 'commune in total have been processed\n', delay, "has been necessary to this step" )
+print(Errors)
 
-    alphabet = alphabet[1:]  # alphabet list incrementation
-    saveAlphabet(alphabet)
+
