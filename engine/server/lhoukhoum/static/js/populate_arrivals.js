2611873e31e505fe2592bc7c7d3605821fb9ab85
lhoumeau <maxime>
2020-05-07 14:50:51 +0200

    change route and display of index.html adding api call

--- engine/server/lhoukhoum/static/js/populate_arrivals.js
+++ engine/server/lhoukhoum/static/js/populate_arrivals.js
@@ -96,40 +96,42 @@ $(document).on('change','#trip_date',function() {
     month = date.getMonth() + 1;
     day = ("0" + day).slice(-2);
     year = date.getFullYear();
-    alert([year, month, day].join('-'));
+    //alert([year, month, day].join('-'));
     let selected_date = [year, month, day].join('-')+'T05:00:00+0200'
     //calculate destination price
-    if($('#check_api').prop("checked") == true){
-        $.ajax({
-            url: "/station/price",
-            type: "POST",
-            data: {
-                'start_station': departure,
-                'stop_station': arrival,
-                'trip_date': selected_date
-            },
-            success: function(response) {
+    $.ajax({
+        url: "/station/price",
+        type: "POST",
+        data: {
+            'start_station': departure,
+            'stop_station': arrival,
+            'trip_date': selected_date
+        },
+        success: function(response) {
 
-                data = response;
-                $("#trip_table").empty()
+            data = response;
+            $("#trip_table").empty()
 
-                for(var i = 0, size = data['arrival_code'].length; i < size ; i++){
-                        var tr = $("<tr id=\"r"+i.toString()+"\" >");
-                        tr.append($("<td width=\"25%\">").text(data['arrival_code'][i]));
-                        tr.append($("<td width=\"25%\">").text(data['price'][i]));
-                        tr.append($("<td width=\"25%\">").text(data['type'][i]));
-                        tr.append($("<td width=\"25%\">").text(data['category'][i]));
-                        $("#trip_table").append(tr);
-                }
-            },
-            error: function(output){
-                console.log('error');
-            },
-        });
-    }
-    else if($('#check_api').prop("checked") == false){
-        alert("pricing api not activated,display map only...");
-    }
+            for(var i = 0, size = data['arrival_code'].length; i < size ; i++){
+                    var tr = $("<tr id=\"r"+i.toString()+"\" >");
+                    tr.append($("<td width=\"25%\">").text(data['arrival_code'][i]));
+                    tr.append($("<td width=\"25%\">").text(data['price'][i]));
+                    tr.append($("<td width=\"25%\">").text(data['type'][i]));
+                    tr.append($("<td width=\"25%\">").text(data['category'][i]));
+
+                    $("#trip_table").append(tr);
+            }
+        },
+        error: function(output){
+            console.log('error');
+        },
+    });
+});
+
+$(document).on('click','#trip_table tr', function() {
+    var city = $("td").first().text();
+    //alert(city)
+    window.location.href='station/destination/'+city.toLowerCase();
 });
 
 
